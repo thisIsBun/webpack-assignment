c1d1eb16b3e90b16c2f8b9a2663f6bbdc9700c29
thisIsBun <bun_101454@castech.com.tw>
2023-06-13 13:12:58 +0800

    setup output.clean

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         filename: 'static/js/[name].[hash].js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     plugins: [
         new MiniCssExtractPlugin({
